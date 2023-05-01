58007da7616418b3ff36a422588202edb30bcf09
QuickerMaths <mateusz.szalowicz@gmail.com>
2023-05-01 15:32:41 +0200

    commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+for (const image of galleryItems) {
+  const listEl = `<li class="gallery__item">
+        <a class="gallery__link" href="${image.original}">
+          <img
+            class="gallery__image"
+            src=${image.preview}
+            data-source=${image.original}
+            alt=${image.description}
+          />
+        </a>
+      </li>`;
+
+  galleryEl.insertAdjacentHTML('beforeend', listEl);
+  galleryEl.addEventListener('click', e => e.preventDefault());
+}
+
+let lightbox = new SimpleLightbox('.gallery li a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+
+let data = {
+  email: '',
+  message: '',
+};
+
+if (localStorage.getItem('feedback-form-state')) {
+  data = JSON.parse(localStorage.getItem('feedback-form-state'));
+  formEl.email.value = data.email;
+  formEl.message.value = data.message;
+}
+
+const getData = e => {
+  data[e.target.name] = e.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+};
+
+formEl.addEventListener('submit', e => {
+  e.preventDefault();
+
+  localStorage.clear();
+  formEl.reset();
+});
+
+formEl.addEventListener('input', throttle(getData, 500));
